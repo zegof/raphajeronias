4425a0c9971c93c0c1060340f3fe7bca28d08b4e
zegof <117827585+zegof@users.noreply.github.com>
2024-10-29 12:28:56 +0100

    Aktualisiere main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,45 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    Zauberer,
-    assets.animation`MagierZaubertAnimation`,
-    1000,
-    false
-    )
-    projectile = sprites.createProjectileFromSprite(assets.image`ProjectileZauberer`, Zauberer, 50, 0)
+    if (ZaubererBildRichtung == 1) {
+        animation.runImageAnimation(
+        Zauberer,
+        assets.animation`MagierZaubertAnimation`,
+        1000,
+        false
+        )
+        projectile = sprites.createProjectileFromSprite(assets.image`LeerBild`, Zauberer, 50, 0)
+        pause(500)
+        projectile.setImage(assets.image`ProjectileZauberer`)
+    } else if (ZaubererBildRichtung == 2) {
+        animation.runImageAnimation(
+        Zauberer,
+        assets.animation`MagierZaubertAnimation`,
+        1000,
+        false
+        )
+        projectile = sprites.createProjectileFromSprite(assets.image`LeerBild`, Zauberer, -50, 0)
+        pause(500)
+        projectile.setImage(assets.image`ProjectileZauberer`)
+    }
 })
 let projectile: Sprite = null
 let Zauberer: Sprite = null
+let ZaubererBildRichtung = 0
+tiles.setCurrentTilemap(tilemap`Level2`)
 let ZaubererBildVar = assets.image`Magier Normal`
-let ZaubererBildRichtung = 1
+ZaubererBildRichtung = 1
 Zauberer = sprites.create(ZaubererBildVar, SpriteKind.Player)
+Zauberer.setScale(0.9, ScaleAnchor.Middle)
 controller.moveSprite(Zauberer, 100, 0)
 game.onUpdate(function () {
-    Zauberer.setImage(ZaubererBildVar)
     if (Zauberer.vx > 0) {
         ZaubererBildVar = assets.image`Magier Normal`
         ZaubererBildRichtung = 1
+        Zauberer.setImage(ZaubererBildVar)
     } else if (Zauberer.vx < 0) {
         if (ZaubererBildRichtung == 1) {
             ZaubererBildVar.flipX()
             ZaubererBildRichtung = 2
         }
+        Zauberer.setImage(ZaubererBildVar)
     }
 })
