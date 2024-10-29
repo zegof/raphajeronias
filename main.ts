9418da65c1c2106a5ea98186b62940c06edaa2d1
zegof <117827585+zegof@users.noreply.github.com>
2024-10-29 08:24:44 +0100

    Aktualisiere main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
-let Zauberer = sprites.create(assets.image`Magier Normal`, SpriteKind.Player)
-animation.runImageAnimation(
-Zauberer,
-assets.animation`MagierZaubertAnimation`,
-200,
-false
-)
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    Zauberer,
+    assets.animation`MagierZaubertAnimation`,
+    1000,
+    false
+    )
+    projectile = sprites.createProjectileFromSprite(assets.image`ProjectileZauberer`, Zauberer, 50, 0)
+})
+let projectile: Sprite = null
+let Zauberer: Sprite = null
+Zauberer = sprites.create(assets.image`Magier Normal`, SpriteKind.Player)
+controller.moveSprite(Zauberer, 100, 0)
+game.onUpdate(function () {
+    if (Zauberer.vx < 0) {
+        Zauberer.setImage(assets.image`Magier Normal`)
+    } else if (Zauberer.vx > 0) {
+        Zauberer.setImage(assets.image`Magier Normal`)
+        assets.image`Magier Normal`.flipX()
+    }
+})
