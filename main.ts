565d2049b6aca5a48ab269d8b7865fbd93da0f16
zegof <117827585+zegof@users.noreply.github.com>
2024-10-29 14:17:06 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         false
         )
         projectile = sprites.createProjectileFromSprite(assets.image`LeerBild`, Zauberer, 50, 0)
+        projectile.setScale(Scale, ScaleAnchor.Middle)
         pause(500)
         projectile.setImage(assets.image`ProjectileZauberer`)
     } else if (ZaubererBildRichtung == 2) {
@@ -17,19 +18,23 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         false
         )
         projectile = sprites.createProjectileFromSprite(assets.image`LeerBild`, Zauberer, -50, 0)
+        projectile.setScale(Scale, ScaleAnchor.Middle)
         pause(500)
         projectile.setImage(assets.image`ProjectileZauberer`)
     }
 })
 let projectile: Sprite = null
+let Scale = 0
 let Zauberer: Sprite = null
 let ZaubererBildRichtung = 0
 tiles.setCurrentTilemap(tilemap`Level2`)
 let ZaubererBildVar = assets.image`Magier Normal`
 ZaubererBildRichtung = 1
 Zauberer = sprites.create(ZaubererBildVar, SpriteKind.Player)
-Zauberer.setScale(0.9, ScaleAnchor.Middle)
+Scale = 0.9
+Zauberer.setScale(Scale, ScaleAnchor.Middle)
 controller.moveSprite(Zauberer, 100, 0)
+scene.cameraFollowSprite(Zauberer)
 game.onUpdate(function () {
     if (Zauberer.vx > 0) {
         ZaubererBildVar = assets.image`Magier Normal`
