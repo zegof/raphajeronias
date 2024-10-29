5d7601d8aaa3a195ebd315437a269e3c4369751c
zegof <117827585+zegof@users.noreply.github.com>
2024-10-29 08:40:15 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,13 +9,19 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 let projectile: Sprite = null
 let Zauberer: Sprite = null
-Zauberer = sprites.create(assets.image`Magier Normal`, SpriteKind.Player)
+let ZaubererBildVar = assets.image`Magier Normal`
+let ZaubererBildRichtung = 1
+Zauberer = sprites.create(ZaubererBildVar, SpriteKind.Player)
 controller.moveSprite(Zauberer, 100, 0)
 game.onUpdate(function () {
-    if (Zauberer.vx < 0) {
-        Zauberer.setImage(assets.image`Magier Normal`)
-    } else if (Zauberer.vx > 0) {
-        Zauberer.setImage(assets.image`Magier Normal`)
-        assets.image`Magier Normal`.flipX()
+    Zauberer.setImage(ZaubererBildVar)
+    if (Zauberer.vx > 0) {
+        ZaubererBildVar = assets.image`Magier Normal`
+        ZaubererBildRichtung = 1
+    } else if (Zauberer.vx < 0) {
+        if (ZaubererBildRichtung == 1) {
+            ZaubererBildVar.flipX()
+            ZaubererBildRichtung = 2
+        }
     }
 })
